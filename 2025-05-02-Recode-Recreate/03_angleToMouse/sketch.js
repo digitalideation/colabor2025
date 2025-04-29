902713f4fc875a863f2ef26c5c075d3de4665bf2
hzuellig <zuellig@internauta.ch>
2025-04-29 18:15:34 +0200

    exp atan2

---
+++ 2025-05-02-Recode-Recreate/03_angleToMouse/sketch.js
@@ -0,0 +1,53 @@
+let xx = 0;
+let yy = 0;
+
+let tilesX = 20;
+let tilesY = 20;
+let gridX, gridY;
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  gridX = width / tilesX;
+  gridY = height / tilesY;
+
+
+
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+
+  // Use mouse position data
+  xx = mouseX;
+  yy = mouseY;
+
+
+  drawGrid();
+}
+
+
+function drawGrid() {
+  for (let y = 0; y < tilesY; y++) {
+    for (let x = 0; x < tilesX; x++) {
+      //Rechne den Winkel zur Maus
+      let winkel = atan2(yy - (y * gridY + gridY / 2), xx - (x * gridX + gridX / 2));
+      if (winkel < 0) { winkel += 2 * PI; }
+
+      push();
+      translate(x * gridX + gridX / 2, y * gridY + gridY / 2);
+      rotate(winkel);
+
+
+      line(0, 0, gridX / 2, 0);
+
+
+      pop();
+
+    }
+
+  }
+}
